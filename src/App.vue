2ada703a44e7bca7c094cd10d53b28fdb0548f4b
llz <lianglunzhong@gmail.com>
2019-08-13 16:41:15 +0800

    添加注册页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="wrap">
     <TheHeader/>
+
+    <div id="main-container" class="container main-container">
+      <router-view/>
+    </div>
+
     <TheFooter/>
   </div>
 </template>
