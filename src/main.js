ec24728ae2919b7ac1c26360132bd3fc2f97a641
llz <lianglunzhong@gmail.com>
2019-08-14 16:03:15 +0800

    注册完善 - 添加 vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import './directives'
 import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -15,6 +17,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -85,7 +85,9 @@
             password: this.password,
             avatar: `https://api.adorable.io/avatars/200/${this.username}.png`
           }
-          const localUser = ls.getItem('user')
+
+          // 从仓库获取用户信息
+          const localUser = this.$store.state.user
 
           if (localUser) {
             if (localUser.name === user.name) {
@@ -99,7 +101,9 @@
         }
       },
       login(user) {
-        ls.setItem('user', user)
+        // 分发 login 事件，以保存用户信息和登录
+        this.$store.dispatch('login', user)
+
         this.showMsg('注册成功', 'success')
       },
       showMsg(msg, type = 'warning') {
