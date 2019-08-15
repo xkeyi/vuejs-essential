57e5843d4be3d2e9e616ba5475afbabe751fac4b
llz <lianglunzhong@gmail.com>
2019-08-15 15:31:36 +0800

    封装提示插件

--- src/views/users/Profile.vue
+++ src/views/users/Profile.vue
@@ -97,7 +97,8 @@ export default {
             hobbies: this.hobbies,
             introduction: this.introduction
           })
-          alert('修改成功')
+
+          this.$message.show('修改成功')
         }
       })
     }
