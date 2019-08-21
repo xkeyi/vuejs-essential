d7aeea0d647b4ad9bfb7b94332b32f9e30e979ec
llz <lianglunzhong@gmail.com>
2019-08-21 14:31:09 +0800

    帖子过滤

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,9 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           <ul class="list-inline topic-filter">
-            <li><a href="/topics?filter=default" class="active">活跃</a></li>
-            <li><a href="/topics?filter=excellent">精华</a></li>
-            <li><a href="/topics?filter=vote">投票</a></li>
-            <li><a href="/topics?filter=recent">最近</a></li>
-            <li><a href="/topics?filter=noreply">零回复</a></li>
+            <li v-for="item in filters">
+              <router-link v-title="item.title" :class="{ active: filter === item.filter }" :to="`/topics?filter=${item.filter}`">{{ item.name }}</router-link>
+            </li>
           </ul>
           <div class="clearfix"></div>
         </div>
@@ -56,7 +54,16 @@ export default {
     return {
       msg: '', // 消息
       msgType: '', // 消息类型
-      msgShow: false // 是否显示消息，默认不显示
+      msgShow: false, // 是否显示消息，默认不显示
+      articles: [], // 文章列表
+      filter: 'default', // 默认过滤方式
+      filters: [ // 过滤方式列表
+        { filter: 'default', name: '活跃', title: '最后回复排序'},
+        { filter: 'excellent', name: '精华', title: '只看加精的话题'},
+        { filter: 'vote', name: '投票', title: '点赞数排序'},
+        { filter: 'recent', name: '最近', title: '发布时间排序'},
+        { filter: 'noreply', name: '零回复', title: '无人问津的话题'}
+      ],
     }
   },
   // 组件内的路由导航守卫
@@ -83,6 +90,9 @@ export default {
       } else if (logout) {
         vm.showMsg('操作成功')
       }
+
+      // 确认渲染该组件的对应路由时，设置相关数据
+      vm.setDataByFilter(to.query.filter)
     })
   },
   computed: {
@@ -90,9 +100,6 @@ export default {
       'auth',
       'user',
     ]),
-    articles() {
-      return this.$store.getters.computedArticles
-    }
   },
   watch: {
     // 监听 auth，它的值变为 false 时，显示操作成功提示，因为在首页退出时，路由没有发生改变，不会触发 beforeRouteEnter
@@ -100,6 +107,10 @@ export default {
       if (!value) {
         this.showMsg('操作成功')
       }
+    },
+    // 监听 '$route'，在查询参数变化后，设置相关数据
+    '$route'(to) {
+      this.setDataByFilter(to.query.filter)
     }
   },
   methods: {
@@ -107,6 +118,13 @@ export default {
       this.msg = msg
       this.msgType = type
       this.msgShow = true
+    },
+    // 设置相关数据
+    setDataByFilter(filter = 'default') {
+      // 设置当前过滤方式为查询参数的 filter
+      this.filter = filter
+      // 设置文章列表为过滤后的所有文章
+      this.articles = this.$store.getters.getArticlesByFilter(filter)
     }
   }
 }
