0dc6c74deedcd7a559e16b7ce240038bd11fa2a2
llz <lianglunzhong@gmail.com>
2019-08-21 14:02:57 +0800

    添加帖子列表

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import store from './store'
 import VueSweetalert2 from './plugins/vue-sweetalert2'
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 // 使用插件
 Vue.use(VueSweetalert2)
@@ -21,6 +23,27 @@ Vue.config.productionTip = false
 // eslint 配置，允许 new 一个实例后不赋值，我们没有使用 eslint，如果有，则下一行注释不可缺少
 /* eslint-disable no-new */
 
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -29,6 +52,6 @@ new Vue({
   components: { App },
   template: '<App/>',
   created() {
-    console.log(this.$options.el) // => '#app'
+    // console.log(this.$options.el) // => '#app'
   }
 })
